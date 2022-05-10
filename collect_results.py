64e0c69209b5da2f01b051d1f17fce6a6176138d
albertomancino <alberto.mancino94@gmail.com>
2022-05-10 17:30:01 +0200

    stats computing

---
+++ collect_results.py
@@ -0,0 +1,79 @@
+import os
+import pandas as pd
+import tqdm
+
+names = ['Random', 'MostPop', 'ItemKNN', 'EASER', 'RP3beta']
+
+
+def get_result_parameters(folder_name):
+    params = folder_name.split('_')
+    name = params[0]
+    gen = params[1].replace('gen', '')
+    eps = params[2].replace('eps', '')
+    return name, gen, eps
+
+def map_models_names(models):
+    new_names = []
+    for c in models:
+        for n in names:
+            if n in c:
+                new_names.append(n)
+    return new_names
+
+def write_on_files(path_and_line):
+    for p, l in path_and_line:
+        with open(p, 'a') as file:
+            file.writelines(l)
+
+dataset = 'AmazonDigitalMusic'
+sub_folder = 'results'
+output_folder = 'stats'
+output_files = 'performance_{dataset}_{model}.tsv'
+
+output_files_path = {n : os.path.join(output_folder, output_files.format(model=n, dataset=dataset)) for n in names}
+
+directory_path = os.path.join('data', dataset, sub_folder)
+files_path = os.listdir(directory_path)
+
+if os.path.isdir(output_folder) is False:
+    os.makedirs(output_folder)
+
+# write header
+fp = files_path[0]
+performance_folder = os.path.join(directory_path, fp, 'performance')
+intra_results_folders = os.listdir(performance_folder)
+result_file = None
+for files in intra_results_folders:
+    if 'rec_cutoff' in files:
+        result_file = os.path.join(performance_folder, files)
+assert result_file is not None
+data = pd.read_csv(result_file, sep='\t', header=0)
+cols = ['dataset', 'model', 'generation', 'epsilon'] + list(data.columns)[1:]
+cols_to_string = '\t'.join(cols) + '\n'
+cols_to_file = [(p, cols_to_string) for p in output_files_path.values()]
+write_on_files(cols_to_file)
+
+for fp in tqdm.tqdm(files_path):
+    result_per_model = {m : '' for m in names}
+    result_parameters = get_result_parameters(fp)
+    performance_folder = os.path.join(directory_path, fp, 'performance')
+    intra_results_folders = os.listdir(performance_folder)
+    result_file = None
+    for files in intra_results_folders:
+        if 'rec_cutoff' in files:
+            result_file = os.path.join(performance_folder, files)
+    assert result_file is not None
+
+    data = pd.read_csv(result_file, sep='\t', header=0)
+    data.model = map_models_names(data.model)
+    data['dataset'] = result_parameters[0]
+    data['generation'] = result_parameters[1]
+    data['epsilon'] = result_parameters[2]
+
+    to_write = []
+    for row in data[cols].values:
+        model = row[1]
+        string_row = [str(el) for el in row]
+        row_to_write = '\t'.join(string_row) + '\n'
+        to_write.append((output_files_path[model], row_to_write))
+    write_on_files(to_write)
